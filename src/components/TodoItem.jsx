435fd3dfeb488c9dbad20598ec82e484582292d8
Deepthi <deepthireddy20200@gmail.com>
2025-11-10 21:57:29 +0530

    Fix component casing issues

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,26 @@
-export default function TodoItem({ todo, onToggle, onDelete }) {export default function TodoItem({ todo, onToggle, onDelete }) {
+export default function TodoItem({ todo, onToggle, onDelete }) {export default function TodoItem({ todo, onToggle, onDelete }) {export default function TodoItem({ todo, onToggle, onDelete }) {
 
-  return (  return (
+  return (
 
-    <li className={`todo-item ${todo.done ? 'done' : ''}`}>    <li className={`todo-item ${todo.done ? 'done' : ''}`}>
+    <li className={`todo-item ${todo.done ? 'done' : ''}`}>  return (  return (
 
-      <span onClick={() => onToggle(todo.id)}>      <span onClick={() => onToggle(todo.id)}>
+      <span onClick={() => onToggle(todo.id)}>
 
-        {todo.text}        {todo.text}
+        {todo.text}    <li className={`todo-item ${todo.done ? 'done' : ''}`}>    <li className={`todo-item ${todo.done ? 'done' : ''}`}>
 
-      </span>      </span>
+      </span>
 
+      <button onClick={() => onDelete(todo.id)} className="delete-btn">      <span onClick={() => onToggle(todo.id)}>      <span onClick={() => onToggle(todo.id)}>
+
+        ×
+
+      </button>        {todo.text}        {todo.text}
+
+    </li>
+
+  );      </span>      </span>
+
+}
       <button onClick={() => onDelete(todo.id)} className="delete-btn">      <button onClick={() => onDelete(todo.id)} className="delete-btn">
 
         ×        ×
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,34 @@
-import TodoItem from './TodoItem';import TodoItem from './TodoItem';
+import TodoItem from './TodoItem';import TodoItem from './TodoItem';import TodoItem from './TodoItem';
 
 
 
-export default function TodoList({ todos, onToggle, onDelete }) {export default function TodoList({ todos, onToggle, onDelete }) {
+export default function TodoList({ todos, onToggle, onDelete }) {
 
-  return (  return (
+  return (
 
-    <ul className="todo-list">    <ul className="todo-list">
+    <ul className="todo-list">export default function TodoList({ todos, onToggle, onDelete }) {export default function TodoList({ todos, onToggle, onDelete }) {
 
-      {todos.map(todo => (      {todos.map(todo => (
+      {todos.map(todo => (
 
-        <TodoItem        <TodoItem
+        <TodoItem  return (  return (
 
-          key={todo.id}          key={todo.id}
+          key={todo.id}
 
+          todo={todo}    <ul className="todo-list">    <ul className="todo-list">
+
+          onToggle={onToggle}
+
+          onDelete={onDelete}      {todos.map(todo => (      {todos.map(todo => (
+
+        />
+
+      ))}        <TodoItem        <TodoItem
+
+    </ul>
+
+  );          key={todo.id}          key={todo.id}
+
+}
           todo={todo}          todo={todo}
 
           onToggle={onToggle}          onToggle={onToggle}
